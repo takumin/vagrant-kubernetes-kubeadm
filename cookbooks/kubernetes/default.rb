f92feb3c1d03b213c511586b13cd6be3f06f7cb6
takumin <takumiiinn@gmail.com>
2019-04-05 13:19:43 +0900

    kubernetes add command completion and code comment

--- cookbooks/kubernetes/default.rb
+++ cookbooks/kubernetes/default.rb
@@ -1,5 +1,5 @@
 #
-# Package
+# Repository
 #
 
 apt_keyring 'Google Cloud Packages Automatic Signing Key <gc-team@google.com>' do
@@ -25,6 +25,30 @@ execute 'apt-get update' do
   action :nothing
 end
 
+#
+# Package
+#
+
 package 'kubelet'
 package 'kubeadm'
 package 'kubectl'
+
+#
+# Package
+#
+
+%w(kubeadm kubectl).each do |cmd|
+  unless File.exist?("/etc/bash_completion.d/#{cmd}") then
+    local_ruby_block "#{cmd} completion bash" do
+      block do
+        result = run_command(["#{cmd}", 'completion', 'bash'])
+
+        if result.success? then
+          File.open("/etc/bash_completion.d/#{cmd}", 'w', 0644) do |f|
+            f.write(result.stdout)
+          end
+        end
+      end
+    end
+  end
+end
